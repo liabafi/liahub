d7e298dcc2b7a2bf0164bea702df7ecd5cf82da4
Lia Batlle Figueras <liaxiaotao.batlle@gracia.lasalle.cat>
2024-03-06 13:18:59 +0100

    Check if the commit account chnages.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:html' as html;
 import 'dart:js';
 
+// Lia Batlle
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
